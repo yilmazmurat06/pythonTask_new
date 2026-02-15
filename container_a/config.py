e6745985e67d238689105ecb8d38c40192b64aed
yilmazmurat06 <mrtylmz2307@gmail.com>
2026-02-16 00:26:16 +0300

    initial commit

---
+++ container_a/config.py
@@ -0,0 +1,14 @@
+import os
+
+# config dosyası
+
+class Config:
+    def __init__(self):
+        self.rabbitmq_host = os.getenv('RABBITMQ_HOST', 'localhost')
+        self.rabbitmq_port = int(os.getenv('RABBITMQ_PORT', 5672))
+        self.rabbitmq_user = os.getenv('RABBITMQ_PASS', 'guest')
+        self.rabbitmq_pass = os.getenv('RABBITMQ_PASS', 'guest')
+        self.scrape_interval = int(os.getenv('SCRAPE_INTERVAL', 300))
+        self.queue_name = 'interpol_queue'
+
+# env dosyasında ilgili bilgiler yoksa ikinci parametreleri kullanır (os.getenv)
